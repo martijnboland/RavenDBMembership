45ab532e2199c8ff36d15b043369e184ce5dec0b
fschwiet <fschwiet@gmail.com>
2011-02-02 22:34:06 -0800

    trying to figure out SQL Express deployment

---
+++ src/RavenDBMembership.IntegrationTests/can_load_database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using NJasmine;
+
+namespace RavenDBMembership.IntegrationTests
+{
+    public class can_load_database : GivenWhenThenFixture
+    {
+        public override void Specify()
+        {
+            then("the database can be loaded", delegate
+            {
+                var connection = arrange(() => new SqlConnection(Properties.Settings.Default.MembershipDatabase));
+                connection.Open();
+                var command = arrange(() => new SqlCommand("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'", connection));
+
+                command.ExecuteNonQuery();
+            });
+        }
+    }
+}
