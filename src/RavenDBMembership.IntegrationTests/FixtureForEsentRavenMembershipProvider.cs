a70b938d7a996f08025cf79f1c719b4bab8e38db
fschwiet <fschwiet@gmail.com>
2011-02-08 22:23:23 -0800

    and tests can be ran against esent

--- src/RavenDBMembership.IntegrationTests/FixtureForEsentRavenMembershipProvider.cs
+++ src/RavenDBMembership.IntegrationTests/FixtureForEsentRavenMembershipProvider.cs
@@ -1,6 +1,33 @@
-﻿namespace RavenDBMembership.IntegrationTests
+﻿using System;
+using System.IO;
+using System.Web.Security;
+using Raven.Client.Client;
+using Raven.Client.Document;
+using Raven.Database.Config;
+using RavenDBMembership.Provider;
+
+namespace RavenDBMembership.IntegrationTests
 {
-    public class FixtureForEsentRavenMembershipProvider
+    public class FixtureForEsentRavenMembershipProvider : MembershipProviderFixture
     {
+        public override MembershipProvider GetProvider()
+        {
+            string dataDirectory = Path.Combine(Properties.Settings.Default.AccessibleTempPath, "RavenDBMembershipTest.Munin");
+
+            if (Directory.Exists(dataDirectory))
+                Directory.Delete(dataDirectory);
+
+            var store = new EmbeddableDocumentStore()
+            {
+                RunInMemory = false,
+                DataDirectory = dataDirectory
+            };
+            store.Configuration.DefaultStorageTypeName = "Raven.Storage.Esent.TransactionalStorage, Raven.Storage.Esent";
+
+            return new RavenDBMembershipProviderThatDisposesStore()
+            {
+                DocumentStore = store
+            };
+        }
     }
 }
